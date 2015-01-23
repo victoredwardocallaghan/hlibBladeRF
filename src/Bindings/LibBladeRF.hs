5f70e5b71c6bc75719ace20b6984a0cfc1f3c5c5
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2015-01-24 09:00:10 +1100

    src/Bindings/LibBladeRF.hs: add header
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

--- src/Bindings/LibBladeRF.hs
+++ src/Bindings/LibBladeRF.hs
@@ -1,3 +1,15 @@
+{-|
+  Module      : $Header$
+  Copyright   : (c) 2014 Edward O'Callaghan
+  License     : LGPL-2.1
+  Maintainer  : eocallaghan@alterapraxis.com
+  Stability   : provisional
+  Portability : portable
+
+  This module collects together libbladeRF binded FFI
+  C functions into a common namespace.
+-}
+
 module Bindings.LibBladeRF (module X) where
 
 import Bindings.LibBladeRF.LibBladeRF as X
