f9365f68ad3170e73211fce5302eb62a41db89dc
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2015-02-07 07:56:48 +1100

    examples/info/BladeRFInfo.hs: update example
    Update example to correspond with previous commit.
    
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

--- examples/info/BladeRFInfo.hs
+++ examples/info/BladeRFInfo.hs
@@ -12,13 +12,14 @@ import Control.Monad.IO.Class
 import LibBladeRF.LibBladeRF
 import LibBladeRF.Utils
 import LibBladeRF.Misc
+import LibBladeRF.Types
 
 main  = withBladeRF $ do
-  (backend, serial, usb_bus, usb_addr, inst) <- bladeRFGetDevInfo
-  printBladeRF $ " Backend : " ++ backend
-  printBladeRF $ " Serial #: " ++ serial
-  printBladeRF $ " USB bus: " ++ (show usb_bus)
-  printBladeRF $ " USB address: " ++ (show usb_addr)
-  printBladeRF $ " Instance: " ++ (show inst)
+  info <- bladeRFGetDevInfo
+  printBladeRF $ " Backend : " ++ backend info
+  printBladeRF $ " Serial #: " ++ serial info
+  printBladeRF $ " USB bus: " ++ (show (usbBus info))
+  printBladeRF $ " USB address: " ++ (show (usbAddr info))
+  printBladeRF $ " Instance: " ++ (show (inst info))
   foo <- bladeRFDeviceSpeed
   printBladeRF $ " Device Speed: " ++ (show foo)
