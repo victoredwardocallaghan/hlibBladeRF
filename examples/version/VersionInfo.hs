f89a817ea4911629e96b22224d6857e5d40ba96a
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2015-02-07 06:13:37 +1100

    examples/version/VersionInfo.hs: update example
    Update example to correspond with previous commit.
    
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

--- examples/version/VersionInfo.hs
+++ examples/version/VersionInfo.hs
@@ -5,11 +5,17 @@ import Control.Monad.IO.Class
 import LibBladeRF.LibBladeRF
 import LibBladeRF.Utils
 import LibBladeRF.Misc
+import LibBladeRF.Types
 
 main  = withBladeRF $ do
   libVersion <- liftIO $ bladeRFLibVersion
   fwVersion <- bladeRFFwVersion
   fpgaVersion <- bladeRFFPGAVersion
-  printBladeRF $ " libbladeRF version: " ++ libVersion
+  printBladeRF $ " libbladeRF version: " ++ (show libVersion)
+  let version = show (major libVersion) ++ "." ++
+                show (minor libVersion) ++ "." ++
+                show (patch libVersion) ++ " (" ++
+                     (descr libVersion) ++ ")"
+  printBladeRF $ " bladeRF library version " ++ version
   printBladeRF $ " Firmware version: " ++ fwVersion
   printBladeRF $ " FPGA version: " ++ fpgaVersion
